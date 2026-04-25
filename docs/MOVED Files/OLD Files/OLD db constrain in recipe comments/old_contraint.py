6447721ba37b0c8ac98ac75ecd5ebd49898a522d
dfd-learning <dfd.learning@gmail.com>
2026-04-25 21:57:39 +0000

    Borrar varios archivos

--- docs/MOVED Files/OLD Files/OLD db constrain in recipe comments/old_contraint.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import CheckConstraint
-
-
-# Prevent self-referencing at deeper than one level (now enforced at DB layer)
-### Note: This constraint is PostgreSQL-specific. For other DBs, enforce via app logic or triggers.
-
-CheckConstraint("parent_comment_id IS NULL OR (SELECT c.parent_comment_id FROM comments c WHERE c.id = parent_comment_id) IS NULL", name='check_no_deep_comment_nesting')
